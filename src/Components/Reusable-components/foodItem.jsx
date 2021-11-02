59c20099733b378ea0a9c8a6cb8cd68fc7042bfa
Fahid Khan <fahid.khan@tuni.fi>
2021-11-02 18:44:34 +0200

    Modal and Modal functionality

--- src/Components/Reusable-components/foodItem.jsx
+++ src/Components/Reusable-components/foodItem.jsx
@@ -1,9 +1,32 @@
 import { Card, CardDeck, Container } from "react-bootstrap";
 import { EmojiHeartEyesFill } from "react-bootstrap-icons";
 import { useHistory } from "react-router";
+import React, {useState} from 'react';
+import ModalConfirm from "../Modal/index";
 
 export default function FootItems({ data }) {
   const history = useHistory();
+  
+  const [textModal, setTextModal] = useState('');
+  const [modalBack, setModalBack] = useState(false);
+  const [visibleModal, setVisibleModal] = useState(false);
+  const [item, setItem] = useState({});
+
+  const clickClose = () => {
+    setTextModal('');
+    setModalBack(false);
+    setVisibleModal(false);
+  }
+
+  const clickConfirm = () => {
+    console.log("CONMFIRMED")
+  }
+
+  const onItemClick = (i) => {
+    console.log(i)
+    setItem(i)
+    setVisibleModal(true);
+  }
 
   return (
     <Container>
@@ -19,7 +42,7 @@ export default function FootItems({ data }) {
         {data.map((i, index) => {
           const { id, image, item, ingredient } = i;
           return (
-            <Card key={index} style={{ width: "20rem", margin: "0.7rem" ,border:"none" }}>
+            <Card key={index} style={{ width: "20rem", margin: "0.7rem" ,border:"none" }} onClick={()=>onItemClick(i)} >
               {image === "" ? (
                 <EmojiHeartEyesFill
                   style={{ color: "#000", cursor: "pointer" }}
@@ -54,6 +77,16 @@ export default function FootItems({ data }) {
           );
         })}
       </CardDeck>
+      <ModalConfirm
+        textModal={textModal}
+        modalBack={modalBack}
+        visibleModal={visibleModal}
+        data={item}
+        itemList={data}
+        clickClose={clickClose}
+        clickConfirm={clickConfirm}
+      />
     </Container>
+    
   );
 }
