acf84c31a1fee5b378e6b11d164c8ed2c0ea779b
masood ahmadi <masood ahmadi>
2021-11-07 21:27:31 +0200

    navbar language button and cart icon added

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Icon from "../Images/Icon.svg";
 import { useHistory } from "react-router";
-import { Container, Navbar, Nav } from "react-bootstrap";
-
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  SplitButton,
+} from "react-bootstrap";
+import { Cart } from "react-bootstrap-icons";
 function Header() {
   const history = useHistory();
   return (
@@ -23,12 +29,71 @@ function Header() {
             <strong>Reindeer</strong>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
-            <Nav>
-              <Nav.Link onClick={() => history.push("/login")}>login</Nav.Link>
-              <Nav.Link eventKey={2} onClick={() => history.push("/register")}>
+            <Nav className="ml-auto paddingLeft5" pullRight>
+              {/* <Nav.Link
+                style={{ width: "60px", backgroundColor: "dark" }}
+                onClick={() => history.push("/login")}
+              >
+                login
+              </Nav.Link> */}
+              <Nav.Link
+                style={{ width: "60px", backgroundColor: "dark" }}
+                eventKey={2}
+                onClick={() => history.push("/register")}
+                className="float-right"
+              >
                 register
+              </Nav.Link>
+              {/* <Nav.Link
+                style={{ width: "40px", backgroundColor: "dark" }}
+                eventKey={2}
+                className="float-right"
+              >
+                en
+              </Nav.Link> */}
+              {/* <NavDropdown
+                alignRight
+                style={{ width: "50px", backgroundColor: "dark" }}
+                title="en"
+                id="basic-nav-dropdown-pull-righ"
+              >
+                <NavDropdown.Item
+                  style={{ width: "50px", backgroundColor: "dark" }}
+                  href="#action/3.1"
+                >
+                  fi
+                </NavDropdown.Item>
+              </NavDropdown> */}
+              <NavDropdown
+                alignLeft
+                title="en"
+                id="basic-nav-dropdown"
+                style={{ width: "60px", marginLeft: "20px" }}
+              >
+                <NavDropdown.Item
+                  style={{ right: "auto", left: "auto" }}
+                  href="#action/3.1"
+                >
+                  fi
+                </NavDropdown.Item>{" "}
+              </NavDropdown>
+
+              <Nav.Link
+                className="float-end"
+                style={{ width: "50px", backgroundColor: "dark" }}
+                eventKey={2}
+                onClick={() => history.push("/Cart")}
+              >
+                <Cart size={25} />
+              </Nav.Link>
+              <Nav.Link
+                style={{ width: "60px", backgroundColor: "dark" }}
+                onClick={() => history.push("/login")}
+              >
+                login
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
