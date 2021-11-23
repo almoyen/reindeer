378dbff50d9251e085dee429a2897d191b51928d
masood ahmadi <masood@eduix.it>
2021-11-23 18:26:11 +0200

    spinner laoding while fetch does not work

--- src/Components/Reusable-components/loader.jsx
+++ src/Components/Reusable-components/loader.jsx
@@ -5,7 +5,11 @@ export default function Loader() {
   return (
     <div>
       <Spinner
-        style={{ marginLeft: "50vw", marginTop: "30vh", fontSize: "200px" }}
+        style={{
+          marginLeft: "50vw",
+          marginTop: "30vh",
+          fontSize: "200px",
+        }}
         animation="border"
         variant="warning"
         name="loading..."
