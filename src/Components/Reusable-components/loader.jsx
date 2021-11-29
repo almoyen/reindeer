24a121f573b57023d5f4c8dbc93dd526d5a659e6
masood ahmadi <masood@eduix.it>
2021-11-29 11:39:41 +0200

    loading icon margin fixed

--- src/Components/Reusable-components/loader.jsx
+++ src/Components/Reusable-components/loader.jsx
@@ -1,19 +1,19 @@
-import { React } from "react";
-import { Spinner } from "react-bootstrap";
+import { React } from 'react'
+import { Spinner } from 'react-bootstrap'
 
 export default function Loader() {
   return (
     <div>
       <Spinner
         style={{
-          marginLeft: "50vw",
-          marginTop: "30vh",
-          fontSize: "200px",
+          marginLeft: '47vw',
+          marginTop: '33vh',
+          fontSize: '200px',
         }}
         animation="border"
         variant="warning"
         name="loading..."
       />
     </div>
-  );
+  )
 }
